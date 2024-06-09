aca508ab46bd6c9d5c1cf021785bbb13d19dc869
Quanquan Liu <qliu2011@gmail.com>
2024-06-09 16:17:17 -0400

    fixed batch deletions bug

--- benchmarks/BatchDynamicConnectivity/Connectivity/Connectivity.h
+++ benchmarks/BatchDynamicConnectivity/Connectivity/Connectivity.h
@@ -353,10 +353,11 @@ struct Connectivity {
         bool abort = false;
 
         while(non_empty_spanning_tree) {
-            std::cout << "started round" << std::endl;
             if (first) {
                 edges_both_directions =  sequence<std::pair<uintE, uintE>>(2 * deletions.size());
-                auto split_edges = sequence<SkipList::SkipListElement*>(deletions.size(), nullptr);
+                auto split_edges =
+                    sequence<std::pair<uintE, uintE>>(deletions.size(),
+                            std::make_pair(UINT_E_MAX, UINT_E_MAX));
                 auto is_split_edge = sequence<bool>(deletions.size(), false);
 
                 parallel_for(0, deletions.size(), [&](size_t i) {
@@ -367,11 +368,14 @@ struct Connectivity {
                     auto edge_index = edge_table.find(std::make_pair(u, v), UINT_E_MAX);
                     if (edge_index != UINT_E_MAX && tree.edge_table[edge_index].twin != nullptr) {
                         is_split_edge[i] = true;
-                        split_edges[i] = &tree.edge_table[edge_index];
+                        split_edges[i] = std::make_pair(
+                            tree.edge_table[edge_index].id.first,
+                            tree.edge_table[edge_index].id.second);
                     }
                 });
 
-                auto edges_to_split = parlay::pack(split_edges, is_split_edge);
+                sequence<std::pair<uintE, uintE>>
+                    edges_to_split = parlay::pack(split_edges, is_split_edge);
 
                 auto compare_tup = [&] (const std::pair<uintE, uintE> l, const std::pair<uintE, uintE> r) {
                     return l.first < r.first;
@@ -399,10 +403,9 @@ struct Connectivity {
                         tree.skip_list.find_representative(our_vertex);
                 });
 
-                tree.skip_list.batch_split(&edges_to_split);
+                tree.batch_cut(edges_to_split, edge_table);
             }
 
-            std::cout << "finished first" << std::endl;
             first = false;
 
             if (!first) {
@@ -414,7 +417,6 @@ struct Connectivity {
             }
 
             parlay::sort_inplace(parlay::make_slice(representative_nodes));
-            std::cout << "finished sort representative nodes" << std::endl;
 
             auto bool_seq = sequence<bool>(representative_nodes.size());
             parallel_for(0, representative_nodes.size(), [&](size_t i) {
@@ -422,7 +424,6 @@ struct Connectivity {
                        (edges_both_directions[i-1] != edges_both_directions[i]);
             });
             auto representative_starts = parlay::pack_index(bool_seq);
-            std::cout << "finished representative starts" << std::endl;
 
             sequence<std::pair<uintE, uintE>> found_possible_edges =
                     sequence<std::pair<uintE, uintE>>(representative_starts.size());
